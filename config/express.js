698a44c5b939c59f6270ffd3cebde6a198e5e7be
Jason S. Jones <jsjones96@gmail.com>
2014-11-06 06:02:47 -0800

    add some comments to config/express.js

--- config/express.js
+++ config/express.js
@@ -9,6 +9,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
+// make this function available to all other modules
+// that require it.  This returns a handle to the
+// app created by the call to express().  app is used
+// the server.js file to actually start the server.
 module.exports = function() {
     var app = express();
     var apiRouter = express.Router();
@@ -18,14 +22,20 @@ module.exports = function() {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
+    // require the routes, passing the objects they need
+    // to define the routes on.
     require('../routes/index.server.routes')(app);
     require('../routes/atm.server.routes')(apiRouter);
 /*
+    // define middleware for all routes.  This middleware
+    // simply logs out the request method and url
     app.use(function(req, res, next) {
         console.log(req.method + ' request for ' + req.url);
         next();
     });
 */
+    // http://baseurl.com/api is the root for all routes
+    // defined by the apiRouter.
     app.use('/api', apiRouter);
 
     return app;
