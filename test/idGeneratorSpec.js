67b5e15ca0bb7f03feca8c96b4299a896d7f8f9f
Jason S. Jones <jsjones96@gmail.com>
2014-11-08 22:03:38 -0800

    add id generator utility with unit tests

---
+++ test/idGeneratorSpec.js
@@ -0,0 +1,54 @@
+var should = require('should');
+var idGenerator = require('../util/id-generator');
+
+describe('The id generator', function () {
+
+    describe('with no initial seed parameter', function () {
+        var sut = null;
+
+        beforeEach(function () {
+            sut = new idGenerator();
+        })
+
+        it('initializes starting id number to 0', function () {
+            sut.should.be.defined;
+            sut.getNextId().should.equal(0);
+        });
+
+        it('three calls to getNextId returns 2', function () {
+            sut.should.be.defined;
+            sut.getNextId();
+            sut.getNextId();
+            var nextId = sut.getNextId();
+            nextId.should.equal(2);
+        });
+    });
+
+    describe('with an initial parameter of 10', function () {
+        var sut = null;
+
+        beforeEach(function () {
+            sut = new idGenerator(10);
+        });
+
+        it('initializes starting id number to 10', function () {
+            sut.should.be.defined;
+            sut.getNextId().should.equal(10);
+        });
+
+        it('five calls to getNextId returns 14', function () {
+            sut.should.be.defined;
+            sut.getNextId();
+            sut.getNextId();
+            sut.getNextId();
+            sut.getNextId();
+            var nextId = sut.getNextId();
+            nextId.should.equal(14);
+        });
+
+        it('sets id to 100 when resetId is called with param 100', function () {
+            sut.resetId(100);
+            sut.getNextId().should.equal(100);
+        });
+    });
+});
---
+++ util/id-generator.js
@@ -0,0 +1,24 @@
+var idGenerator = function(seed) {
+
+    var idNumber = seed || 0;
+
+    var id = { };
+
+    id.getNextId = function() {
+        var nextId = idNumber;
+        incrementId();
+        return nextId;
+    };
+
+    id.resetId = function(newId) {
+        idNumber = newId;
+    };
+
+    var incrementId = function() {
+        idNumber += 1;
+    };
+
+    return id;
+}
+
+module.exports = idGenerator;
