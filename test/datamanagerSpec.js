bc0f866360115d3a2ccb767d5c03212885384da1
Jason S. Jones <jsjones96@gmail.com>
2014-11-09 06:47:54 -0800

    refactor test structure for datamanager util

--- test/datamanagerSpec.js
+++ test/datamanagerSpec.js
@@ -16,43 +16,47 @@ after(function () {
 
 describe('The datamanager module', function() {
 
-    it('should read in a file', function() {
-        data.should.be.ok;
-    });
+    describe('read functionality', function () {
 
-    it('and the contents of the file should be an array with length 3',
-        function() {
-            json.should.be.an.Array;
-            json.should.have.length(3);
-    });
-
-    it('the first element of array should have a cashAmount property',
-        function() {
-            json[0].should.have.property('cashAmount');
-    });
-
-    it('and a serviceFee property', function() {
-        json[0].should.have.property('serviceFee');
-    })
-});
-
-describe('The datamanager module', function() {
-    it('should write to a file', function(done) {
-        var testObj = {
-            "msg": "this is to test the write functionality"
-        };
-        dm.writeDataToFile(outputFile, testObj)
-        fs.exists(outputFile, function(exists) {
-            exists.should.be.ok;
-            done();
+        it('should read in a file', function() {
+            data.should.be.ok;
         });
+
+        it('and the contents of the file should be an array with length 3',
+            function() {
+                json.should.be.an.Array;
+                json.should.have.length(3);
+        });
+
+        it('the first element of array should have a cashAmount property',
+            function() {
+                json[0].should.have.property('cashAmount');
+        });
+
+        it('and a serviceFee property', function() {
+            json[0].should.have.property('serviceFee');
+        })
     });
 
-    it('and read the file it just wrote', function() {
-        var contents = dm.getDataFromFile(outputFile);
-        var outputJSON = JSON.parse(contents);
+    describe('write functionality', function () {
 
-        contents.should.be.ok
-        outputJSON.should.have.property('msg');
+        it('should write to a file', function(done) {
+            var testObj = {
+                "msg": "this is to test the write functionality"
+            };
+            dm.writeDataToFile(outputFile, testObj)
+            fs.exists(outputFile, function(exists) {
+                exists.should.be.ok;
+                done();
+            });
+        });
+
+        it('and read the file it just wrote', function() {
+            var contents = dm.getDataFromFile(outputFile);
+            var outputJSON = JSON.parse(contents);
+
+            contents.should.be.ok
+            outputJSON.should.have.property('msg');
+        });
     });
 });
