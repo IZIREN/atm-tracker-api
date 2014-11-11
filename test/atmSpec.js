04a452bb2a8d5fda0a4247a4a8fa9485868a502e
Jason S. Jones <jsjones96@gmail.com>
2014-11-11 06:15:21 -0800

    add space after function keyword in anonymous fns

--- test/atmSpec.js
+++ test/atmSpec.js
@@ -1,34 +1,34 @@
 var should = require('should');
 var ATM = require('../models/atm');
 
-describe('ATM object', function() {
+describe('ATM object', function () {
 
-    describe('defaults', function() {
+    describe('default', function () {
 
         var atm = {};
 
-        before(function() {
+        before(function () {
 
             atm = new ATM({ id: 999 });
         });
 
-        it('id is 999', function() {
+        it('id is 999', function () {
             atm.id.should.equal(999);
         });
 
-        it('has a cashAmount', function() {
+        it('has a cashAmount', function () {
             atm.cashAmount.should.be.defined;
         });
 
-        it('has a serviceFee', function() {
+        it('has a serviceFee', function () {
             atm.serviceFee.should.be.defined;
         });
 
-        it('has a dateOfTransaction', function() {
+        it('has a dateOfTransaction', function () {
             atm.dateOfTransaction.should.be.defined;
         });
 
-        it('has a purchases array', function() {
+        it('has a purchases array', function () {
             atm.purchases.should.be.defined;
             atm.purchases.should.be.an.Array;
         });
--- test/datamanagerSpec.js
+++ test/datamanagerSpec.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var data, json;
 var outputFile = './data/testOutput.json';
-before(function() {
+before(function () {
     data = dm.getDataFromFile('./data/data.json');
     json = JSON.parse(data);
     console.log('BEFORE: json data read and parsed');
@@ -14,44 +14,44 @@ after(function () {
     console.log('AFTER: test file removed');
 });
 
-describe('The datamanager module', function() {
+describe('The datamanager module', function () {
 
     describe('read functionality', function () {
 
-        it('should read in a file', function() {
+        it('should read in a file', function () {
             data.should.be.ok;
         });
 
         it('and the contents of the file should be an array with length 3',
-            function() {
+            function () {
                 json.should.be.an.Array;
                 json.should.have.length(3);
         });
 
         it('the first element of array should have a cashAmount property',
-            function() {
+            function () {
                 json[0].should.have.property('cashAmount');
         });
 
-        it('and a serviceFee property', function() {
+        it('and a serviceFee property', function () {
             json[0].should.have.property('serviceFee');
         })
     });
 
     describe('write functionality', function () {
 
-        it('should write to a file', function(done) {
+        it('should write to a file', function (done) {
             var testObj = {
                 "msg": "this is to test the write functionality"
             };
             dm.writeDataToFile(outputFile, testObj)
-            fs.exists(outputFile, function(exists) {
+            fs.exists(outputFile, function (exists) {
                 exists.should.be.ok;
                 done();
             });
         });
 
-        it('and read the file it just wrote', function() {
+        it('and read the file it just wrote', function () {
             var contents = dm.getDataFromFile(outputFile);
             var outputJSON = JSON.parse(contents);
 
