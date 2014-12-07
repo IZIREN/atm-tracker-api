89052e5473020c9dc68d2303d0c7ed631a1e72a9
Jason S. Jones <jsjones96@gmail.com>
2014-12-07 06:31:37 -0800

    add lint to default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-    console.log('Hello World from gulp!');
-});
+gulp.task('default', ['lint']);
 
 gulp.task('lint', function() {
     return gulp.src([ 'app/**/*.js', 'config/**/*.js', 'util/**/*.js'])
